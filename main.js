e21a5458a9d97ed1363f262756db0b66317797f7
Omar Navarro <omar.navarroy@yahoo.com>
2021-01-09 22:56:04 -0800

    Added dynamic list in index.html

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 const links = [
   {
-    label: "Week1 notes",
-    url: "week1/index.html"
+    label: "Week 1",
+    url: "week01/story_editor.html"
+  },
+  {
+    label: "Week 2",
+    url: "week02/index.html"
   }
 ]
+
+var ul = document.querySelector("ul");
+
+for (var i = 0; i < links.length; i++) {
+  var linkLabel = links[i].label;
+  var linkUrl = links[i].url
+  var str = '<a href="' + linkUrl + '">' + linkLabel + '</a>' 
+  var listItem = document.createElement("li");
+
+  listItem.innerHTML = str
+
+  ul.appendChild(listItem);
+}
