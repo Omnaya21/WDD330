4e62e42f070240ab7de76253c21fcecf11bb753d
Omar Navarro <onavarro@ingyso.com>
2021-03-21 01:44:48 -0700

    Added node modules to project in week 11

--- main.js
+++ main.js
@@ -1,75 +1,108 @@
 const links = [
   {
-    label: "Week 10 Team Activity",
-    url: "week10/team10/index.html"
-  },
-  {
-    label: "Week 10 Validation 1",
-    url: "week10/validation/index.html"
-  },
-  {
-    label: "Week 10 Validation 2",
-    url: "week10/validation/index2.html"
-  },
-  {
     label: "Week 1",
-    url: "week01/story_editor.html"
+    url: "week01/story_editor.html",
+    updated: "2021-01-02"
   },
   {
     label: "Week 2",
-    url: "week02/teamw2.html"
+    url: "week02/teamw2.html",
+    updated: "2021-03-19",
+    updated: "2021-01-09"
   },
   {
     label: "Week 3",
-    url: "week03/team03.html"
+    url: "week03/team03.html",
+    updated: "2021-03-19",
+    updated: "2021-01-16"
   },
   {
     label: "Week 4 Quiz",
-    url: "week04/quiz.html"    
+    url: "week04/quiz.html",
+    updated: "2021-01-23"
   },
   {
     label: "Week 4 Team Activity",
-    url: "week04/team04.html"
+    url: "week04/team04.html",
+    updated: "2021-01-23"
   },
   {
     label: "Week 5 Team Activity",
-    url: "week05/finalhikes/index.html"
+    url: "week05/finalhikes/index.html",
+    updated: "2021-01-30"
   },
   {
     label: "Week 6 Challenge One: ToDo",
-    url: "week06/todo/index.html"
+    url: "week06/todo/index.html",
+    updated: "2021-02-06"
   },
   {
     label: "Week 7 Team activity: Hikes + comments",
-    url: "week07/hikes/index.html"
+    url: "week07/hikes/index.html",
+    updated: "2021-02-13"
   },
   {
     label: "Week 7 Quiz Ninja Exercise",
-    url: "week07/quiz/index.html"
+    url: "week07/quiz/index.html",
+    updated: "2021-02-13"
   },
   {
     label: "Week 8 Canvas Demo",
-    url: "week08/anim/index.html"
+    url: "week08/anim/index.html",
+    updated: "2021-02-27"
   },
   {
     label: "Week 8 Team activity: Star Wars API",
-    url: "week08/index.html"
+    url: "week08/index.html",
+    updated: "2021-02-27"
   },
   {
     label: "Week 9 Team activity: Drum Kit",
-    url: "week09/javascript30-drums/index-START.html"
+    url: "week09/javascript30-drums/index-START.html",
+    updated: "2021-03-06"
   },
   {
     label: "Week 9 Quiz Ninja Exercise",
-    url: "week09/quizninja/index.html"
+    url: "week09/quizninja/index.html",
+    updated: "2021-03-06"
   },
+  {
+    label: "Week 10 Team Activity",
+    url: "week10/team10/index.html",
+    updated: "2021-03-13"
+  },
+  {
+    label: "Week 10 Validation 1",
+    url: "week10/validation/index.html",
+    updated: "2021-03-13"
+  },
+  {
+    label: "Week 10 Validation 2",
+    url: "week10/validation/index2.html",
+    updated: "2021-03-13"
+  },
+  {
+    label: "Week 11 Team Activity",
+    url: "week11/team10/index.html",
+    updated: "2021-03-19"
+  }
 ]
+
+links.sort(function(a, b) {
+  var keyA = new Date(a.updated),
+    keyB = new Date(b.updated);
+  // Compare the 2 dates
+  if (keyA < keyB) return 1;
+  if (keyA > keyB) return -1;
+  return 0;
+});
 
 var cards = document.querySelector(".cards");
 
+sortedArray = links.sort();
 for (var i=0; i<links.length; i++) {
-  var linkLabel = links[i].label;
-  var linkUrl = links[i].url;
+  var linkLabel = sortedArray[i].label;
+  var linkUrl = sortedArray[i].url;
 
   var str = '<div class="card-info">';
   str += '<h2><a href="' + linkUrl + '">' + linkLabel + '</a></h2>' ;
